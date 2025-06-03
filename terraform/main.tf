f869e04a4a519e654bba8b33773f0b241752a2c7
katrennada <34171769+katrennada@users.noreply.github.com>
2025-06-03 18:49:22 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,13 @@
-# Terraform placeholder (exemple de VPC, à adapter)
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_version = ">= 1.1.9"
+}
+
+provider "null" {
+  # Provider null, on peut simuler des actions ou appeler des scripts
+}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo 'Terraform resource applied!'"
+  }
 }
